6077a38a1e9f1fcd1951031467ed9729431201a0
cq <continuousqa@gmail.com>
2017-08-13 15:33:23 -0700

    added 4 concurrent go routines

--- bannerNabber.go
+++ bannerNabber.go
@@ -5,24 +5,26 @@ import (
 	"fmt"
 	"net"
 	"time"
+	"sync"
 )
 
 var host string
 var start_port int
 var end_port int
+var wg sync.WaitGroup
 
 func main() {
 	user_input()
 }
 
-func check_port(host string, start_port, end_port int) {
+func check_port_set1(host string, start_port, end_port int) {
 
 	for i := start_port; i <= end_port; i++ {
 		//fmt.Println(i)
 		qualified_host := fmt.Sprintf("%s%s%d", host, ":", i)
 		conn, err := net.DialTimeout("tcp", qualified_host, 50*time.Millisecond)  // Got the timeout code from: https://stackoverflow.com/questions/37294052/golang-why-net-dialtimeout-get-timeout-half-of-the-time
 		if err != nil {
-			//fmt.Println(err)
+			//fmt.Println("set1: ", err)
 			continue
 		}
 		fmt.Fprintf(conn, "GET / HTTP/1.0\r\n\r\n1\n22\n\n\n\n")
@@ -31,6 +33,62 @@ func check_port(host string, start_port, end_port int) {
 		status, err := bufio.NewReader(conn).ReadString('\n')
 		fmt.Println(i, status)
 	}
+	wg.Done()
+}
+
+func check_port_set2(host string, start_port, end_port int) {
+
+	for i := start_port; i <= end_port; i++ {
+		//fmt.Println(i)
+		qualified_host := fmt.Sprintf("%s%s%d", host, ":", i)
+		conn, err := net.DialTimeout("tcp", qualified_host, 50*time.Millisecond)  // Got the timeout code from: https://stackoverflow.com/questions/37294052/golang-why-net-dialtimeout-get-timeout-half-of-the-time
+		if err != nil {
+			//fmt.Println("set2: ", err)
+			continue
+		}
+		fmt.Fprintf(conn, "GET / HTTP/1.0\r\n\r\n1\n22\n\n\n\n")
+		conn.SetReadDeadline(time.Now().Add(50*time.Millisecond))
+
+		status, err := bufio.NewReader(conn).ReadString('\n')
+		fmt.Println(i, status)
+	}
+	wg.Done()
+}
+func check_port_set3(host string, start_port, end_port int) {
+
+	for i := start_port; i <= end_port; i++ {
+		//fmt.Println(i)
+		qualified_host := fmt.Sprintf("%s%s%d", host, ":", i)
+		conn, err := net.DialTimeout("tcp", qualified_host, 50*time.Millisecond)  // Got the timeout code from: https://stackoverflow.com/questions/37294052/golang-why-net-dialtimeout-get-timeout-half-of-the-time
+		if err != nil {
+			//fmt.Println("set3: ", err)
+			continue
+		}
+		fmt.Fprintf(conn, "GET / HTTP/1.0\r\n\r\n1\n22\n\n\n\n")
+		conn.SetReadDeadline(time.Now().Add(50*time.Millisecond))
+
+		status, err := bufio.NewReader(conn).ReadString('\n')
+		fmt.Println(i, status)
+	}
+	wg.Done()
+}
+func check_port_set4(host string, start_port, end_port int) {
+
+	for i := start_port; i <= end_port; i++ {
+		//fmt.Println(i)
+		qualified_host := fmt.Sprintf("%s%s%d", host, ":", i)
+		conn, err := net.DialTimeout("tcp", qualified_host, 50*time.Millisecond)  // Got the timeout code from: https://stackoverflow.com/questions/37294052/golang-why-net-dialtimeout-get-timeout-half-of-the-time
+		if err != nil {
+			//fmt.Println("set4: ", err)
+			continue
+		}
+		fmt.Fprintf(conn, "GET / HTTP/1.0\r\n\r\n1\n22\n\n\n\n")
+		conn.SetReadDeadline(time.Now().Add(50*time.Millisecond))
+
+		status, err := bufio.NewReader(conn).ReadString('\n')
+		fmt.Println(i, status)
+	}
+	wg.Done()
 }
 
 func user_input() {
@@ -41,5 +99,24 @@ func user_input() {
 	fmt.Println("End Port (i.e. 8080)> ")
 	fmt.Scan(&end_port)
 	fmt.Println("Running scan... ")
-	check_port(host, start_port, end_port)
+
+	//check_port_set1(host, start_port, end_port) // 15s to run 1000 ports sequentially
+	port_range := end_port - start_port
+	//end_port_set1 := (port_range / 2) + start_port
+	end_port_set1 := (port_range / 4) + start_port
+	end_port_set2 := (port_range / 4) + end_port_set1
+	end_port_set3 := (port_range / 4) + end_port_set2
+
+	//wg.Add(2)		// 13s to run 1000 ports on 2 concurrent groups
+	//go check_port_set1(host, start_port, end_port_set1)
+	//go check_port_set2(host, (end_port_set1 + 1), end_port)
+	//wg.Wait()
+
+	wg.Add(4)		// 9s to run 1000 ports on 4 concurrent groups
+	go check_port_set1(host, start_port, end_port_set1)
+	go check_port_set2(host, (end_port_set1 + 1), end_port_set2)
+	go check_port_set3(host, (end_port_set2 + 1), end_port_set3)
+	go check_port_set4(host, (end_port_set3 + 1), end_port)
+	wg.Wait()
+
 }
